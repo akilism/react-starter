88d7b208bb03155daa05fa670f39caf10c16e4e8
Akil Harris <akil.harris@firstlook.org>
2016-08-21 18:09:04 -0400

    renders on both sides.

--- src/js/components/Root.js
+++ src/js/components/Root.js
@@ -1,19 +1,8 @@
 import React from "react";
 
-const testImg = require("../../assets/test.jpg");
-
-
-const Root = ({ children, text = "some default text" }) =>
+export default ({ children, text = "some default text" }) =>
   <div>
     <h1>Hello, { text }</h1>
-    <img src={ testImg } alt="test" />
+    <img src="/assets/test.jpg" alt="test" />
     { children }
   </div>;
-
-export default Root;
-
-// export default class extends React.Component {
-//   render() {
-//     return <h1>Hello World</h1>;
-//   }
-// }
